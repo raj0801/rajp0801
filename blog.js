c6e0cb028d9070acb5fa433ef42de1e86388bdc7
Rajkumar <pr1650644@gmail.com>
2022-07-02 12:54:34 +0530

    changed blog file

--- blog.js
+++ blog.js
@@ -1,9 +1,16 @@
-var more=document.getElementById("more");
-var btn=document.getElementById("btn");
+var more = document.getElementById("more");
+var btn = document.getElementById("btn");
 
-console.log("hello");
-function myfunction(){
-more.classList.toggle('hide');
-btn.innerHTML = more.classList.contains('hide') ? "Read More" : "Read Less" ;
+
+function myfunction() {
+    more.classList.toggle('hide');
+    btn.innerHTML = more.classList.contains('hide') ? "Read More" : "Read Less";
 }
-// hello new line
+
+var more1 = document.getElementById("more-1");
+var btn1 = document.getElementById("btn-1");
+
+function myfunction1() {
+    more1.classList.toggle('hide-1');
+    btn1.innerHTML=more1.classList.contains('hide-1') ? "Read More" : "Read Less";
+}
