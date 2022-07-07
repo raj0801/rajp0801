8535ddc213986e71dcdafe6661162bf03b1cbe75
Rajkumar <pr1650644@gmail.com>
2022-07-07 12:27:32 +0530

    changed file

--- blog.js
+++ blog.js
@@ -1,3 +1,9 @@
+var d=new Date();
+document.querySelector("#date").innerHTML=d.toLocaleString('default',{day:'numeric',month:'long',year:'numeric'});
+
+var d1=new Date();
+document.querySelector("#date1").innerHTML=d1.toLocaleString('default',{day:'numeric',month:'long',year:'numeric'});
+
 var more = document.getElementById("more");
 var btn = document.getElementById("btn");
 
@@ -11,6 +17,6 @@ var more1 = document.getElementById("more-1");
 var btn1 = document.getElementById("btn-1");
 
 function myfunction1() {
-    more1.classList.toggle('hide-1');
-    btn1.innerHTML=more1.classList.contains('hide-1') ? "Read More" : "Read Less";
+    more1.classList.toggle('hide');
+    btn1.innerHTML=more1.classList.contains('hide') ? "Read More" : "Read Less";
 }
